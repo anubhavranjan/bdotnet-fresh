6c4ea4a29da5cab620f1f8afd64507ff3a9796f2
Anubhav Ranjan <atulanubhav05@gmail.com>
2018-10-04 00:54:24 +0530

    Fix for fetching Description

--- themes/Hugo Fresh/static/js/speaker.js
+++ themes/Hugo Fresh/static/js/speaker.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $("#speaker-submit").click(function(){
         var name = $("#speaker-name").val();
         var email = $("#speaker-email").val();
-        var topic = $("#speaker-description").text();
+        var topic = $("#speaker-description").val();
         var jsonData = {name: name, email: email, topic: topic};
         var postData = JSON.stringify(jsonData);
         alert(postData);
